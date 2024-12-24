a5ec9c5cbbf59d4c8169c8117fc26ca7ffc4eb02
Samuel Mehalko <samuel.mehalko@gmail.com>
2024-12-24 18:42:37 -0500

    Brute force, but takes way too long

--- 2024/10/day10.py
+++ 2024/10/day10.py
@@ -1,5 +1,7 @@
 # --- Day 10: Hoof It ---
 
+from tqdm import tqdm
+
 possible_directions = []
 for i1 in "UDLR":
     for i2 in "UDLR":
@@ -13,7 +15,7 @@ for i1 in "UDLR":
                                     possible_directions.append(i1 + i2 + i3 + i4 + i5 + i6 + i7 +i8 + i9)
 
 trails = []
-with open("input.txt", "r") as file:
+with open("input_example.txt", "r") as file:
     for line in file:
         trails.append(list(line.rstrip()))
 
@@ -36,5 +38,26 @@ def do_it_connect(point1: tuple, point2: tuple, direction: str):
                 point1[0] = point1[0] - 1
                 continue 
         else:
-            return false
+            return False
     return point1 == point2
+
+def find_points_of_interest(trails):
+    zeros = []
+    nines = []
+    for idx, line in enumerate(trails):
+        for jdx, point in enumerate(line):
+            if '0' == point:
+                zeros.append((idx, jdx))
+            if '9' == point:
+                nines.append((idx, jdx))
+    return zeros, nines
+
+
+score = 0
+zeros, nines = find_points_of_interest(trails)
+for direction in tqdm(possible_directions):
+    for zero in zeros:
+        for nine in nines:
+            if do_it_connect(zero, nine, possible_directions):
+                score += 1
+print(score)
